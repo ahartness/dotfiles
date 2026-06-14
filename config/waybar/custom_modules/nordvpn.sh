755d499b0d36f75f6fe347a68bd71b795d0c5e3d
Andrew Hartness <hartness.andrew@gmail.com>
2026-06-14 14:28:42 -0400

    Rearrange some of the scripting for waybar

---
+++ config/waybar/custom_modules/nordvpn.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Get status from nordvpn
+status=$(nordvpn status | grep "Status" | cut -d ':' -f2 | xargs)
+
+CONNECTED_COLOR="#50fa7b"
+DISCONNECTED_COLOR="#bf616a"
+
+if [ "$status" = "Connected" ]; then
+  city=$(nordvpn status | grep "City" | cut -d ':' -f2 | xargs)
+  transfer=$(nordvpn status | grep "Transfer" | cut -d ':' -f2 | xargs)
+
+  #Output for Json for waybar
+  echo "{\"text\": \" | <span color='$CONNECTED_COLOR'>⏻  VPN Connected</span>: $city, $transfer\", \"class\": \"connected\", \"tooltip\": \"$status\"}"
+else
+  echo "{\"text\": \" | <span color='$DISCONNECTED_COLOR'>⏻  VPN Disconnected</span>\", \"class\": \"disconnected\", \"tooltip\": \"$status\"}"
+fi
