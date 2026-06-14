566f6902ba685ec7374b2db39e9ab3b47afdf98e
Andrew Hartness <hartness.andrew@gmail.com>
2026-06-14 14:36:34 -0400

    Fixes for nordvpn statuses

--- config/waybar/custom_modules/nordvpn.sh
+++ config/waybar/custom_modules/nordvpn.sh
@@ -11,7 +11,7 @@ if [ "$status" = "Connected" ]; then
   transfer=$(nordvpn status | grep "Transfer" | cut -d ':' -f2 | xargs)
 
   #Output for Json for waybar
-  echo "{\"text\": \" | <span color='$CONNECTED_COLOR'>⏻  VPN Connected</span>: $city, $transfer\", \"class\": \"connected\", \"tooltip\": \"$status\"}"
+  echo "{\"text\": \" | <span color='$CONNECTED_COLOR'> ⏻  VPN Connected</span>: $city, $transfer\", \"class\": \"connected\", \"tooltip\": \"$status\"}"
 else
-  echo "{\"text\": \" | <span color='$DISCONNECTED_COLOR'>⏻  VPN Disconnected</span>\", \"class\": \"disconnected\", \"tooltip\": \"$status\"}"
+  echo "{\"text\": \" | <span color='$DISCONNECTED_COLOR'> ⏻  VPN Disconnected</span>\", \"class\": \"disconnected\", \"tooltip\": \"$status\"}"
 fi
