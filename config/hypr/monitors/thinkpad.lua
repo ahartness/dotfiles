4d1c132ff0c305b460005d5d469e9b565d0c06f2
Andrew Hartness <hartness.andrew@gmail.com>
2026-07-14 19:26:38 -0400

    Update thinkpad config

--- config/hypr/monitors/thinkpad.lua
+++ config/hypr/monitors/thinkpad.lua
@@ -1,6 +1,6 @@
 -- Thinkpad t14 Gen 3 setup
 hl.monitor({
-    output   = "eDP-1",
-    mode     = "2240x1400@60.002",
-    scale    = "1.25",
+	output = "eDP-1",
+	mode = "2240x1400@60.002",
+	scale = "1.25",
 })
